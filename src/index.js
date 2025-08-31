851f4b64ce312e9dc5765c0d2bfcc1df0ab42f75
macair <macair@GektorJackson.local>
2025-08-31 20:15:22 +0400

    feat: added function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const ones = [
+    "zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine"
+  ];
+
+  const teens = [
+    "ten", "eleven", "twelve", "thirteen", "fourteen",
+    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+  ];
+
+  const tens = [
+    "", "", "twenty", "thirty", "forty",
+    "fifty", "sixty", "seventy", "eighty", "ninety"
+  ];
+
+  if (number === 0) return "zero";
+
+  let result = "";
+
+  if (Math.floor(number / 100) > 0) {
+    result += ones[Math.floor(number / 100)] + " hundred";
+    number %= 100;
+    if (number > 0) result += " ";
+  }
+
+  if (number >= 10 && number < 20) {
+    result += teens[number - 10];
+  } else {
+    if (Math.floor(number / 10) > 0) {
+      result += tens[Math.floor(number / 10)];
+      if (number % 10 > 0) result += " ";
+    }
+    if (number % 10 > 0) {
+      result += ones[number % 10];
+    }
+  }
+
+  return result;
 };
