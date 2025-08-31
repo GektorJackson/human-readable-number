ecd1c4251bb1f605cf8819e84fdd5fccd34934e9
macair <macair@GektorJackson.local>
2025-08-31 20:35:23 +0400

    feat: fix funnction

--- src/index.js
+++ src/index.js
@@ -1,38 +1,63 @@
 module.exports = function toReadable(number) {
   const ones = [
-    "zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine"
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
   ];
 
   const teens = [
-    "ten", "eleven", "twelve", "thirteen", "fourteen",
-    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
   ];
 
   const tens = [
-    "", "", "twenty", "thirty", "forty",
-    "fifty", "sixty", "seventy", "eighty", "ninety"
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
   ];
 
-  if (number === 0) return "zero";
+  if (number === 0) return 'zero';
 
-  let result = "";
+  let result = '';
+  let n = number;
 
-  if (Math.floor(number / 100) > 0) {
-    result += ones[Math.floor(number / 100)] + " hundred";
-    number %= 100;
-    if (number > 0) result += " ";
+  if (Math.floor(n / 100) > 0) {
+    result += `${ones[Math.floor(n / 100)]} hundred`;
+    n %= 100;
+    if (n > 0) result += ' ';
   }
 
-  if (number >= 10 && number < 20) {
-    result += teens[number - 10];
+  if (n >= 10 && n < 20) {
+    result += teens[n - 10];
   } else {
-    if (Math.floor(number / 10) > 0) {
-      result += tens[Math.floor(number / 10)];
-      if (number % 10 > 0) result += " ";
+    if (Math.floor(n / 10) > 0) {
+      result += tens[Math.floor(n / 10)];
+      if (n % 10 > 0) result += ' ';
     }
-    if (number % 10 > 0) {
-      result += ones[number % 10];
+    if (n % 10 > 0) {
+      result += ones[n % 10];
     }
   }
 
